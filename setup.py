d66614a9329ed276f89afb5d8a5b63809eedde33
sharmam123 <61382695+sharmam123@users.noreply.github.com>
2021-08-26 17:17:43 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='myapp',
-    version='0.1',
+    version='0.2',
     py_modules=['myapp'],
     entry_points={
         'console_scripts': ['myapp = myapp:run']
